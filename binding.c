765dac3055f16e4f8aff0c79b07f7a59c5941db3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-08-18 13:04:27 +0200

    Switch to GIFLIB

--- binding.c
+++ binding.c
@@ -2,17 +2,231 @@
 #include <bare.h>
 #include <js.h>
 
-#define STBI_ONLY_GIF
-#define STBI_NO_STDIO
-
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_STATIC
-
-#include <stb_image.h>
+#include "gif.h"
 
 static void
 bare_gif__on_finalize(js_env_t *env, void *data, void *finalize_hint) {
   free(data);
+}
+
+typedef struct {
+  const uint8_t *data;
+  size_t len;
+  size_t offset;
+} bare_gif_reader_t;
+
+typedef struct {
+  bare_gif_reader_t reader;
+
+  GifFileType *file;
+
+  int duration;
+  int timestamp;
+
+  GIFDisposeMethod dispose;
+
+  GIFRect rect;
+
+  GIFPicture frame;
+  GIFPicture current;
+  GIFPicture previous;
+
+  int count;
+
+  int transparent;
+
+  bool done;
+} bare_gif_decoder_t;
+
+static int
+bare_gif__on_read(GifFileType *gif, GifByteType *data, int len) {
+  bare_gif_reader_t *reader = gif->UserData;
+
+  if (reader->offset + len <= reader->len) {
+    memcpy(data, reader->data + reader->offset, len);
+
+    reader->offset += len;
+
+    return len;
+  } else {
+    return 0;
+  }
+}
+
+static inline int
+bare_gif__decoder_init(js_env_t *env, bare_gif_decoder_t *decoder, const uint8_t *data, size_t len) {
+  int err;
+
+  decoder->reader = (bare_gif_reader_t) {data, len, 0};
+
+  GifFileType *file = DGifOpen(&decoder->reader, bare_gif__on_read, &err);
+
+  if (file == NULL) {
+    err = js_throw_error(env, NULL, GIFGetError(file, err));
+    assert(err == 0);
+
+    return -1;
+  }
+
+  decoder->file = file;
+  decoder->duration = 0;
+  decoder->timestamp = 0;
+  decoder->dispose = GIF_DISPOSE_NONE;
+  decoder->count = 0;
+  decoder->transparent = GIF_INDEX_INVALID;
+  decoder->done = false;
+
+  err = GIFPictureInit(&decoder->frame);
+  assert(err == 1);
+
+  err = GIFPictureInit(&decoder->current);
+  assert(err == 1);
+
+  err = GIFPictureInit(&decoder->previous);
+  assert(err == 1);
+
+  return 0;
+}
+
+static inline void
+bare_gif__decoder_destroy(js_env_t *env, bare_gif_decoder_t *decoder) {
+  int err;
+
+  err = DGifCloseFile(decoder->file);
+  assert(err == GIF_OK);
+
+  GIFPictureFree(&decoder->frame);
+  GIFPictureFree(&decoder->current);
+  GIFPictureFree(&decoder->previous);
+}
+
+static inline int
+bare_gif__decoder_read_frame(js_env_t *env, bare_gif_decoder_t *decoder, GIFPicture *picture) {
+  int err;
+
+  GifFileType *file = decoder->file;
+
+  while (!decoder->done) {
+    GifRecordType type;
+
+    if (DGifGetRecordType(file, &type) == GIF_ERROR) goto err;
+
+    switch (type) {
+    case IMAGE_DESC_RECORD_TYPE: {
+      GifImageDesc *const image = &file->Image;
+
+      err = DGifGetImageDesc(file);
+      if (err == GIF_ERROR) goto err;
+
+      if (decoder->count == 0) {
+        if (file->SWidth == 0 || file->SHeight == 0) {
+          image->Left = 0;
+          image->Top = 0;
+
+          file->SWidth = image->Width;
+          file->SHeight = image->Height;
+
+          if (file->SWidth <= 0 || file->SHeight <= 0) {
+            goto err;
+          }
+        }
+
+        decoder->frame.width = file->SWidth;
+        decoder->frame.height = file->SHeight;
+
+        err = GIFPictureAlloc(&decoder->frame);
+        if (err != 1) {
+          file->Error = D_GIF_ERR_NOT_ENOUGH_MEM;
+          goto err;
+        }
+
+        GIFPictureClear(&decoder->frame, NULL);
+
+        err = GIFPictureCopy(&decoder->frame, &decoder->current);
+        if (err != 1) {
+          file->Error = D_GIF_ERR_NOT_ENOUGH_MEM;
+          goto err;
+        }
+
+        err = GIFPictureCopy(&decoder->frame, &decoder->previous);
+        if (err != 1) {
+          file->Error = D_GIF_ERR_NOT_ENOUGH_MEM;
+          goto err;
+        }
+      }
+
+      if (image->Width == 0 || image->Height == 0) {
+        image->Width = file->SWidth;
+        image->Height = file->SHeight;
+      }
+
+      err = GIFReadFrame(file, decoder->transparent, &decoder->rect, &decoder->frame);
+      if (err != 1) goto err;
+
+      GIFBlendFrames(&decoder->frame, &decoder->rect, &decoder->current);
+
+      err = GIFPictureCopy(&decoder->current, picture);
+      if (err != 1) {
+        file->Error = D_GIF_ERR_NOT_ENOUGH_MEM;
+        goto err;
+      }
+
+      decoder->count++;
+
+      GIFDisposeFrame(decoder->dispose, &decoder->rect, &decoder->previous, &decoder->current);
+
+      GIFCopyPixels(&decoder->current, &decoder->previous);
+
+      if (decoder->duration <= 10) decoder->duration = 100;
+
+      decoder->timestamp += decoder->duration;
+
+      decoder->dispose = GIF_DISPOSE_NONE;
+      decoder->duration = 0;
+      decoder->transparent = GIF_INDEX_INVALID;
+
+      return 1;
+    }
+    case EXTENSION_RECORD_TYPE: {
+      int extension;
+      GifByteType *data = NULL;
+
+      err = DGifGetExtension(file, &extension, &data);
+      if (err == GIF_ERROR) goto err;
+
+      if (data == NULL) continue;
+
+      if (extension == GRAPHICS_EXT_FUNC_CODE) {
+        err = GIFReadGraphicsExtension(data, &decoder->duration, &decoder->dispose, &decoder->transparent);
+        if (err != 1) {
+          file->Error = D_GIF_ERR_IMAGE_DEFECT;
+          goto err;
+        }
+      }
+
+      while (data) {
+        err = DGifGetExtensionNext(file, &data);
+        if (err == GIF_ERROR) goto err;
+      }
+
+      break;
+    }
+    case TERMINATE_RECORD_TYPE: {
+      decoder->done = true;
+
+      return 0;
+    }
+    default: {
+      break;
+    }
+    }
+  }
+
+err:
+  err = js_throw_error(env, NULL, GIFGetError(file, err));
+  assert(err == 0);
+
+  return -1;
 }
 
 static js_value_t *
@@ -32,9 +246,20 @@ bare_gif_decode(js_env_t *env, js_callback_info_t *info) {
   err = js_get_typedarray_info(env, argv[0], NULL, (void **) &gif, &len, NULL, NULL);
   assert(err == 0);
 
-  int width, height, channels_in_file;
+  bare_gif_decoder_t decoder;
+  err = bare_gif__decoder_init(env, &decoder, gif, len);
+  assert(err == 0);
 
-  uint8_t *data = stbi_load_from_memory(gif, len, &width, &height, &channels_in_file, 4);
+  GIFPicture picture;
+  err = GIFPictureInit(&picture);
+  assert(err == 1);
+
+  err = bare_gif__decoder_read_frame(env, &decoder, &picture);
+  if (err < 0) return NULL;
+
+  int width = picture.width;
+  int height = picture.height;
+  uint32_t *rgba = picture.rgba;
 
   js_value_t *result;
   err = js_create_object(env, &result);
@@ -56,11 +281,13 @@ bare_gif_decode(js_env_t *env, js_callback_info_t *info) {
   len = width * height * 4;
 
   js_value_t *buffer;
-  err = js_create_external_arraybuffer(env, data, len, bare_gif__on_finalize, NULL, &buffer);
+  err = js_create_external_arraybuffer(env, rgba, len, bare_gif__on_finalize, NULL, &buffer);
   assert(err == 0);
 
   err = js_set_named_property(env, result, "data", buffer);
   assert(err == 0);
+
+  bare_gif__decoder_destroy(env, &decoder);
 
   return result;
 }
---
+++ gif.h
@@ -0,0 +1,361 @@
+// Copyright (c) 2010, Google Inc. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the name of Google nor the names of its contributors may
+//     be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <assert.h>
+#include <gif_lib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define GIF_INDEX_INVALID     -1
+#define GIF_TRANSPARENT_COLOR 0x00000000u
+#define GIF_WHITE_COLOR       0xffffffffu
+#define GIF_TRANSPARENT_MASK  0x01
+#define GIF_DISPOSE_MASK      0x07
+#define GIF_DISPOSE_SHIFT     2
+
+typedef enum GIFDisposeMethod {
+  GIF_DISPOSE_NONE,
+  GIF_DISPOSE_BACKGROUND,
+  GIF_DISPOSE_RESTORE_PREVIOUS
+} GIFDisposeMethod;
+
+typedef struct {
+  const uint8_t *data;
+  size_t len;
+} GIFData;
+
+typedef struct {
+  int width, height;
+  uint32_t *rgba;
+  int stride;
+} GIFPicture;
+
+typedef struct {
+  int x, y, width, height;
+} GIFRect;
+
+typedef struct {
+  uint8_t red, green, blue, alpha;
+} GIFPixel;
+
+static void
+GIFCopyPlane(const uint8_t *src, int src_stride, uint8_t *dst, int dst_stride, int width, int height) {
+  assert(src != NULL && dst != NULL);
+  assert(abs(src_stride) >= width && abs(dst_stride) >= width);
+
+  while (height-- > 0) {
+    memcpy(dst, src, width);
+
+    src += src_stride;
+    dst += dst_stride;
+  }
+}
+
+static void
+GIFCopyPixels(const GIFPicture *const src, GIFPicture *const dst) {
+  assert(src != NULL && dst != NULL);
+  assert(src->width == dst->width && src->height == dst->height);
+
+  GIFCopyPlane((uint8_t *) src->rgba, 4 * src->stride, (uint8_t *) dst->rgba, 4 * dst->stride, 4 * src->width, src->height);
+}
+
+static int
+GIFPictureInit(GIFPicture *const picture) {
+  memset(picture, 0, sizeof(*picture));
+
+  return 1;
+}
+
+static void
+GIFPictureFree(GIFPicture *picture);
+
+static int
+GIFPictureAlloc(GIFPicture *const picture) {
+  GIFPictureFree(picture);
+
+  void *argb;
+  const int width = picture->width;
+  const int height = picture->height;
+  const uint64_t size = (uint64_t) width * height;
+
+  argb = malloc(size * sizeof(*picture->rgba));
+
+  if (argb == NULL) return 0;
+
+  picture->rgba = argb;
+  picture->stride = width;
+
+  return 1;
+}
+
+static void
+GIFPictureFree(GIFPicture *picture) {
+  free(picture->rgba);
+
+  picture->rgba = NULL;
+  picture->stride = 0;
+}
+
+static int
+GIFPictureView(const GIFPicture *src, int x, int y, int width, int height, GIFPicture *dst) {
+  if (src == NULL || dst == NULL) return 0;
+
+  if (src != dst) *dst = *src;
+
+  dst->width = width;
+  dst->height = height;
+
+  dst->rgba = src->rgba + y * src->stride + x;
+  dst->stride = src->stride;
+
+  return 1;
+}
+
+static int
+GIFPictureCopy(const GIFPicture *src, GIFPicture *dst) {
+  int err;
+
+  if (src == NULL || dst == NULL) return 0;
+  if (src == dst) return 1;
+
+  *dst = *src;
+
+  dst->rgba = NULL;
+
+  err = GIFPictureAlloc(dst);
+  if (err != 1) return err;
+
+  GIFCopyPlane((const uint8_t *) src->rgba, 4 * src->stride, (uint8_t *) dst->rgba, 4 * dst->stride, 4 * dst->width, dst->height);
+
+  return 1;
+}
+
+static void
+GIFPictureClearRectangle(GIFPicture *const picture, int x, int y, int width, int height) {
+  const size_t stride = picture->stride;
+
+  uint32_t *dst = picture->rgba + y * stride + x;
+
+  for (int j = 0; j < height; j++, dst += stride) {
+    for (int i = 0; i < width; i++) {
+      dst[i] = GIF_TRANSPARENT_COLOR;
+    }
+  }
+}
+
+static void
+GIFPictureClear(GIFPicture *const picture, const GIFRect *const rect) {
+  if (rect) {
+    GIFPictureClearRectangle(picture, rect->x, rect->y, rect->width, rect->height);
+  } else {
+    GIFPictureClearRectangle(picture, 0, 0, picture->width, picture->height);
+  }
+}
+
+static int
+GIFReadGraphicsExtension(const GifByteType *const data, int *const duration, GIFDisposeMethod *const dispose, int *const transparent) {
+  if (data[0] != 4) return 0;
+
+  const int flags = data[1];
+
+  *duration = (data[2] | (data[3] << 8)) /* 10 ms units */ * 10;
+
+  switch ((flags >> GIF_DISPOSE_SHIFT) & GIF_DISPOSE_MASK) {
+  case 3:
+    *dispose = GIF_DISPOSE_RESTORE_PREVIOUS;
+    break;
+  case 2:
+    *dispose = GIF_DISPOSE_BACKGROUND;
+    break;
+  case 1:
+  case 0:
+  default:
+    *dispose = GIF_DISPOSE_NONE;
+    break;
+  }
+
+  *transparent = (flags & GIF_TRANSPARENT_MASK) ? data[4] : GIF_INDEX_INVALID;
+
+  return 1;
+}
+
+static int
+GIFRemap(const GifFileType *const gif, const uint8_t *const src, int len, int transparent, uint32_t *dst) {
+  const ColorMapObject *const map = gif->Image.ColorMap ? gif->Image.ColorMap : gif->SColorMap;
+
+  if (map == NULL) return 1;
+  if (map->Colors == NULL || map->ColorCount <= 0) return 0;
+
+  const GifColorType *colors = map->Colors;
+
+  for (int i = 0; i < len; i++) {
+    if (src[i] == transparent) {
+      dst[i] = GIF_TRANSPARENT_COLOR;
+    } else if (src[i] < map->ColorCount) {
+      const GifColorType c = colors[src[i]];
+
+      GIFPixel *pixel = (GIFPixel *) &dst[i];
+
+      pixel->red = c.Red;
+      pixel->green = c.Green;
+      pixel->blue = c.Blue;
+      pixel->alpha = 0xff;
+    } else {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+static int
+GIFReadFrame(GifFileType *const gif, int transparent, GIFRect *const rectp, GIFPicture *const picture) {
+  int err;
+
+  GIFPicture view;
+
+  const GifImageDesc *const image = &gif->Image;
+
+  const GIFRect rect = {image->Left, image->Top, image->Width, image->Height};
+
+  const uint64_t memory_needed = 4 * rect.width * (uint64_t) rect.height;
+
+  *rectp = rect;
+
+  if (memory_needed != (size_t) memory_needed || memory_needed > (4ULL << 32)) {
+    return 0;
+  }
+
+  err = GIFPictureView(picture, rect.x, rect.y, rect.width, rect.height, &view);
+  if (err != 1) {
+    gif->Error = D_GIF_ERR_IMAGE_DEFECT;
+    goto err;
+  }
+
+  uint32_t *dst = view.rgba;
+
+  uint8_t *tmp = malloc(rect.width * sizeof(*tmp));
+
+  if (tmp == NULL) {
+    gif->Error = D_GIF_ERR_NOT_ENOUGH_MEM;
+    goto err;
+  }
+
+  if (image->Interlace) {
+    const int offsets[] = {0, 4, 2, 1};
+    const int jumps[] = {8, 8, 4, 2};
+
+    for (int pass = 0; pass < 4; pass++) {
+      const size_t stride = (size_t) view.stride;
+
+      int y = offsets[pass];
+
+      uint32_t *row = dst + y * stride;
+
+      const size_t jump = jumps[pass] * stride;
+
+      for (; y < rect.height; y += jumps[pass], row += jump) {
+        err = DGifGetLine(gif, tmp, rect.width);
+        if (err == GIF_ERROR) goto err;
+
+        err = GIFRemap(gif, tmp, rect.width, transparent, row);
+        if (err != 1) {
+          gif->Error = D_GIF_ERR_IMAGE_DEFECT;
+          goto err;
+        }
+      }
+    }
+  } else {
+    uint32_t *ptr = dst;
+
+    for (int y = 0; y < rect.height; y++, ptr += view.stride) {
+      err = DGifGetLine(gif, tmp, rect.width);
+      if (err == GIF_ERROR) goto err;
+
+      err = GIFRemap(gif, tmp, rect.width, transparent, ptr);
+      if (err != 1) {
+        gif->Error = D_GIF_ERR_IMAGE_DEFECT;
+        goto err;
+      }
+    }
+  }
+
+  free(tmp);
+  return 1;
+
+err:
+  free(tmp);
+  return 0;
+}
+
+static void
+GIFDisposeFrame(GIFDisposeMethod dispose, const GIFRect *const rect, const GIFPicture *const previous, GIFPicture *const current) {
+  assert(rect);
+
+  if (dispose == GIF_DISPOSE_BACKGROUND) {
+    GIFPictureClear(current, rect);
+  } else if (dispose == GIF_DISPOSE_RESTORE_PREVIOUS) {
+    const size_t src_stride = previous->stride;
+    const uint32_t *const src = previous->rgba + rect->x + rect->y * src_stride;
+
+    const size_t dst_stride = current->stride;
+    uint32_t *const dst = current->rgba + rect->x + rect->y * dst_stride;
+
+    assert(previous);
+
+    GIFCopyPlane((uint8_t *) src, (int) (4 * src_stride), (uint8_t *) dst, (int) (4 * dst_stride), 4 * rect->width, rect->height);
+  }
+}
+
+static void
+GIFBlendFrames(const GIFPicture *const src, const GIFRect *const rect, GIFPicture *const dst) {
+  const size_t src_stride = src->stride;
+  const size_t dst_stride = dst->stride;
+
+  assert(src->width == dst->width && src->height == dst->height);
+
+  for (int j = rect->y; j < rect->y + rect->height; j++) {
+    for (int i = rect->x; i < rect->x + rect->width; i++) {
+      const GIFPixel *pixel = (GIFPixel *) &src->rgba[j * src_stride + i];
+
+      if (pixel->alpha != 0) {
+        memcpy(&dst->rgba[j * dst_stride + i], pixel, sizeof(*pixel));
+      }
+    }
+  }
+}
+
+static const char *
+GIFGetError(const GifFileType *const gif, int error) {
+  const char *message = GifErrorString(gif == NULL ? error : gif->Error);
+  if (message == NULL) message = "Unknown error";
+  return message;
+}
